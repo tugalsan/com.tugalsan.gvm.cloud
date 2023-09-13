791e9a451af6d264394e6b557f029afe3809bd00
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-09-13 12:05:55 +0300

    Update Main.java

--- src/main/java/com/tugalsan/gvm/cloud/Main.java
+++ src/main/java/com/tugalsan/gvm/cloud/Main.java
@@ -27,7 +27,7 @@ public class Main {
     //java --enable-preview --add-modules jdk.incubator.vector -jar target/com.tugalsan.gvm.cloud-1.0-SNAPSHOT-jar-with-dependencies.jar    
     public static void main(String[] args) {
         var killer = TS_ThreadSyncTrigger.of();
-        var maxDuration = Duration.ofMinutes(10);
+        var maxExecutionDuration = Duration.ofMinutes(10);
         var win = TS_OsPlatformUtils.isWindows();
         var settings = Settings.of(Settings.pathDefault());
         TGS_ValidatorType1<TS_SHttpHandlerRequest> allow = request -> {
@@ -38,52 +38,16 @@ public class Main {
             return true;
         };
         var stringHandler = TS_SHttpHandlerString.of("/", allow, request -> {
-            //CHOOSE EXECUTOR FROM SERVLET NAME
-            var servletName = request.url.path.fileOrServletName;
-            var fileNameLabel = TGS_Coronator.ofStr()
-                    .anoint(val -> TGS_FileUtilsTur.toSafe(servletName))
-                    .anointIf(TGS_StringUtils::isNullOrEmpty, val -> "home")
-                    .coronate();
-            var filePath = TGS_Coronator.of(Path.class).coronateAs(val -> {
-                if (!win) {
-                    var sh = TS_PathUtils.getPathCurrent_nio(fileNameLabel + ".sh");
-                    if (!TS_FileUtils.isExistFile(sh)) {
-                        request.sendError404("ERROR: sh file not found", sh);
-                        return null;
-                    }
-                    return null;
-                }
-                var bat = TS_PathUtils.getPathCurrent_nio(fileNameLabel + ".bat");
-                if (TS_FileUtils.isExistFile(bat)) {
-                    return bat;
-                }
-                var exe = TS_PathUtils.getPathCurrent_nio(fileNameLabel + ".exe");
-                if (!TS_FileUtils.isExistFile(exe)) {
-                    request.sendError404("ERROR: bat or exe file not found", bat, exe);
-                    return null;
-                }
-                return null;
-            });
-            if (filePath == null) {
+            var pathExecutor = chooseExecutor(win, request);
+            if (pathExecutor == null) {
                 return null;
             }
-            //TODO DB OPS
-            var rowId = 0L; //TODO push request.url.toString() to db, fetch row id
-            //EXECUTE OPS
-            var result = TS_ThreadAsyncAwait.callSingle(killer, maxDuration, kt -> {
-                var process = TS_OsProcess.of(List.of(filePath.toString(), String.valueOf(rowId)));
-                return process.exitValueOk() ? process.output : process.exception.toString();
-            });
-            if (result.hasError()) {
-                request.sendError404("ERROR: execute", result.exceptionIfFailed.get());
+            var rowId = pushUrlAndFetchRowId(request);
+            if (rowId == null) {
                 return null;
             }
-            d.ci("main", "result.resultIfSuccessful", result.resultIfSuccessful.get());
-            //TODO CREATING RETURN OBJ
-            var type = "txt";
-            var out = "todo: value at db:" + result.resultIfSuccessful.get();
-            //RETURN
-            return TGS_Tuple2.of(TGS_FileTypes.findByContenTypePrefix(type), out);
+            var outExecution = execute(killer, maxExecutionDuration, request, pathExecutor, rowId);
+            return createReply(rowId, outExecution);
         });
         TS_SHttpServer.of(
                 TS_SHttpConfigNetwork.of(settings.ip, settings.sslPort),
@@ -91,6 +55,60 @@ public class Main {
                 TS_SHttpConfigHandlerFile.of(settings.fileHandlerServletName, allow, settings.fileHandlerRoot),
                 stringHandler
         );
+    }
+
+    @Deprecated //TODO DB OPS
+    private static TGS_Tuple2<TGS_FileTypes, String> createReply(long rowId, String outExecution) {
+        var outReply = outExecution + " and data of rowId";
+        var type = "txt";
+        return TGS_Tuple2.of(TGS_FileTypes.findByContenTypePrefix(type), outReply);
+    }
+
+    @Deprecated //TODO DB OPS
+    private static Long pushUrlAndFetchRowId(TS_SHttpHandlerRequest request) {
+        return 0L; //TODO push request.url.toString() to db, fetch row id
+    }
+
+    private static String execute(TS_ThreadSyncTrigger killer, Duration maxExecutionDuration, TS_SHttpHandlerRequest request, Path pathExecutor, long rowId) {
+        var result = TS_ThreadAsyncAwait.callSingle(killer, maxExecutionDuration, kt -> {
+            var process = TS_OsProcess.of(List.of(pathExecutor.toString(), String.valueOf(rowId)));
+            return process.exitValueOk() ? process.output : process.exception.toString();
+        });
+        if (result.hasError()) {
+            request.sendError404("ERROR: execute", result.exceptionIfFailed.get());
+            return null;
+        }
+        d.ci("main", "result.resultIfSuccessful", result.resultIfSuccessful.get());
+        return result.resultIfSuccessful.get();
+    }
+
+    private static Path chooseExecutor(boolean win, TS_SHttpHandlerRequest request) {
+        var servletName = request.url.path.fileOrServletName;
+        var fileNameLabel = TGS_Coronator.ofStr()
+                .anoint(val -> TGS_FileUtilsTur.toSafe(servletName))
+                .anointIf(TGS_StringUtils::isNullOrEmpty, val -> "home")
+                .coronate();
+        var filePath = TGS_Coronator.of(Path.class).coronateAs(val -> {
+            if (!win) {
+                var sh = TS_PathUtils.getPathCurrent_nio(fileNameLabel + ".sh");
+                if (!TS_FileUtils.isExistFile(sh)) {
+                    request.sendError404("ERROR: sh file not found", sh);
+                    return null;
+                }
+                return null;
+            }
+            var bat = TS_PathUtils.getPathCurrent_nio(fileNameLabel + ".bat");
+            if (TS_FileUtils.isExistFile(bat)) {
+                return bat;
+            }
+            var exe = TS_PathUtils.getPathCurrent_nio(fileNameLabel + ".exe");
+            if (!TS_FileUtils.isExistFile(exe)) {
+                request.sendError404("ERROR: bat or exe file not found", bat, exe);
+                return null;
+            }
+            return null;
+        });
+        return filePath;
     }
 
 }
